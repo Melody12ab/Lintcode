cb7358fcbdbe2465cd11a13ac94bf09bce672e48
bigface monster <melody12ab@gmail.com>
2018-04-09 17:35:34 +0800

    add 第k大的数

---
+++ kthLargestElement/kthLargestElement.go
@@ -0,0 +1,41 @@
+/**
+ * @param n: An integer
+ * @param nums: An array
+ * @return: the Kth largest element
+ */
+func kthLargestElement (n int, nums []int) int {
+    return findkthLargestElement(n,nums,0,len(nums)-1)
+}
+
+func findkthLargestElement(k int,nums []int,begin,end int) int {
+    if begin==end{
+        return nums[begin]
+    }
+    index:=partition(nums,begin,end+1)
+    //end-index+1=n 为num中第n大的数
+    if end-index+1==k{
+        return nums[index]
+    }else if(end-index+1>k){
+        return findkthLargestElement(k,nums,index+1,end)
+    }else{
+        return findkthLargestElement(k-(end-index+1),nums,begin,index-1)
+    }
+
+}
+
+//begin为初始索引，end为数组元素个数
+func partition(nums []int,begin ,end int) int {
+    pvalue:=nums[begin]
+    i:=begin
+    j:=begin+1
+    for j<end{
+        if nums[j]<pvalue{
+            i++
+            nums[i],nums[j] = nums[j],nums[i]
+        }
+        j++
+    }
+    nums[i],nums[begin] = nums[begin],nums[i]
+    return  i
+}
+
--- readme.md
+++ readme.md
@@ -6,8 +6,10 @@
 |1|[trailingZeros][2]|easy|go|
 |2|[digitCounts][3]|medium|go|
 |3|[uglyNumber][4]|medium|go|
+|5|[kthLargestElement][5]|medium|go|
 
 [1]: https://github.com/Melody12ab/brush_lintcode/tree/master/aplusb
 [2]: https://github.com/Melody12ab/brush_lintcode/tree/master/trailingZeros
 [3]: https://github.com/Melody12ab/brush_lintcode/tree/master/digitCounts
 [4]: https://github.com/Melody12ab/brush_lintcode/tree/master/uglyNumber
+[5]: https://github.com/Melody12ab/brush_lintcode/tree/master/kthLargestElement
